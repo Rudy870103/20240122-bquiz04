b73bfb73e8bd84800c62ed537ef681a0d140b98e
rudy870103 <favoriteinfinite@gmail.com>
2024-01-22 14:49:30 +0800

    完成註冊帳號檢查及新增會員帳號功能

--- front/reg.php
+++ front/reg.php
@@ -3,12 +3,12 @@
     <tr>
         <td class="tt ct">姓名</td>
         <td class="pp"><input type="text" name="name" id="name"></td>
-    </td>
-</tr>
-<tr>
-    <td class="tt ct">帳號</td>
-    <td class="pp"><input type="text" name="acc" id="acc">
-    <button onclick="chkacc()">檢測帳號</button>
+        </td>
+    </tr>
+    <tr>
+        <td class="tt ct">帳號</td>
+        <td class="pp"><input type="text" name="acc" id="acc">
+            <button onclick="chkacc()">檢測帳號</button>
     </tr>
     <tr>
         <td class="tt ct">密碼</td>
@@ -28,18 +28,46 @@
     </tr>
 </table>
 <div class="ct">
-    <button>註冊</button>
-    <button>重置</button>
+    <button onclick="reg()">註冊</button>
+    <button onclick="clean()">重置</button>
 </div>
 <script>
-    function chkacc(){
-        let acc=$("#acc").val();
-        $.get("./api/chk_acc.php",{acc},(res)=>{
-            if(parseInt(res)==1){
-                alert(`此帳號${acc}已被使用`);
-            }else{
-                alert(`此帳號${acc}可以使用`);
+    function reg() {
+        let user = {
+            name: $("#name").val(),
+            acc: $("#acc").val(),
+            pw: $("#pw").val(),
+            tel: $("#tel").val(),
+            addr: $("#addr").val(),
+            email: $("#email").val(),
+        }
+        $.get("./api/chk_acc.php", {
+            acc: user.acc
+        }, (res) => {
+            if (parseInt(res) == 1 || user.acc == 'admin') {
+                alert(`此帳號${user.acc}已被使用`)
+            } else {
+                $.post("./api/reg.php", user, () => {
+                    location.href = '?do=login'
+                })
             }
         })
+    }
+
+    function chkacc() {
+        let acc = $("#acc").val();
+        $.get("./api/chk_acc.php", {
+            acc
+        }, (res) => {
+            if (parseInt(res) == 1 || acc == 'admin') {
+                alert(`此帳號${acc}已被使用`)
+            } else {
+                alert(`此帳號${acc}可以使用`)
+            }
+        })
+    }
+
+    function clean() {
+        $("#name,#acc,#pw,#tel,#addr,#email").val('');
     }
 </script>
