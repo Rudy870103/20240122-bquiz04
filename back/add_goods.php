3286c2294494a995685cd0bd7ba9ab9413055134
rudy870103 <favoriteinfinite@gmail.com>
2024-01-31 15:28:02 +0800

    建立新增商品表單頁面

---
+++ back/add_goods.php
@@ -0,0 +1,48 @@
+<h2 class="ct">新增商品</h2>
+<form action="./api/save_goods" method="post" enctype="multipart/form-data">
+    <table class="all">
+        <tr>
+            <th class="tt ct">所屬大分類</th>
+            <td class="pp">
+                <input type="text" name="big" id="big">
+            </td>
+        </tr>
+        <tr>
+            <th class="tt ct">所屬中分類</th>
+            <td class="pp"><input type="text" name="mid" id="mid"></td>
+        </tr>
+        <tr>
+            <th class="tt ct">商品編號</th>
+            <td class="pp">完成分類後自動分配</td>
+        </tr>
+        <tr>
+            <th class="tt ct">商品名稱</th>
+            <td class="pp"><input type="text" name="name" value=""></td>
+        </tr>
+        <tr>
+            <th class="tt ct">商品價格</th>
+            <td class="pp"><input type="text" name="price" value=""></td>
+        </tr>
+        <tr>
+            <th class="tt ct">規格</th>
+            <td class="pp"><input type="text" name="spec" value=""></td>
+        </tr>
+        <tr>
+            <th class="tt ct">庫存量</th>
+            <td class="pp"><input type="text" name="stock" value=""></td>
+        </tr>
+        <tr>
+            <th class="tt ct">商品圖片</th>
+            <td class="pp"><input type="text" name="img" id=""></td>
+        </tr>
+        <tr>
+            <th class="tt ct">商品介紹</th>
+            <td class="pp"><textarea name="intro" style="width: 80%;height:150px"></textarea></td>
+        </tr>
+    </table>
+    <div class="ct">
+        <input type="submit" value="新增">
+        <input type="reset" value="重置">
+        <input type="button" value="返回">
+    </div>
+</form>
