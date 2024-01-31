6d3aeb707ea3f25fdf15e0773e73dbba9faffa7d
rudy870103 <favoriteinfinite@gmail.com>
2024-01-31 11:21:58 +0800

    完成編輯會員功能

--- back/mem.php
+++ back/mem.php
@@ -4,16 +4,16 @@
         <th class="tt ct">姓名</th>
         <th class="tt ct">會員帳號</th>
         <th class="tt ct">註冊日期</th>
-        <th class="tt ct">管理</th>
+        <th class="tt ct">操作</th>
     </tr>
     <?php
-    $rows=$Admin->all();
+    $rows=$Mem->all();
     foreach($rows as $row){
     ?>
     <tr>
         <td class="pp ct"><?=$row['name'];?></td>
         <td class="pp ct"><?=$row['acc'];?></td>
-        <td class="pp ct"><?$row['regdate'];?></td>
+        <td class="pp ct"><?=$row['regdate'];?></td>
         <td class="pp ct">
             <?php
                 echo "<button onclick='location.href=&#39;?do=edit_mem&id={$row['id']}&#39;'>修改</button>";
