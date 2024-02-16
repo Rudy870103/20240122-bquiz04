04c7cc22b039d2fdb51a74bab0b16ee2365b7348
rudy870103 <favoriteinfinite@gmail.com>
2024-02-16 15:48:53 +0800

    完成點選我要購買,在購物車中顯示商品

--- front/buycart.php
+++ front/buycart.php
@@ -11,6 +11,36 @@ if(!isset($_SESSION['mem'])){
 
     if(!isset($_SESSION['cart'])){
         echo "<h2 class='ct'>購物車中尚無商品</h2>";
-    }else{
-        dd($_SESSION['cart']);
     }
+?>
+    <table class="all">
+        <tr class="tt ct">
+            <td>編號</td>
+            <td>商品名稱</td>
+            <td>數量</td>
+            <td>庫存量</td>
+            <td>單價</td>
+            <td>小計</td>
+            <td>刪除</td>
+        </tr>
+   <?php
+        foreach($_SESSION['cart'] as $id => $qt){
+            $goods=$Goods->find($id);
+        ?>
+        <tr class="pp ct">
+            <td><?=$goods['no'];?></td>
+            <td><?=$goods['name'];?></td>
+            <td><?=$qt;?></td>
+            <td><?=$goods['stock'];?></td>
+            <td><?=$goods['price'];?></td>
+            <td><?=$goods['price'] * $qt;?></td>
+            <td><img src="./icon/0415.jpg" alt=""></td>
+        </tr>
+    <?php
+    }
+    ?>
+    </table>
+
+    <div class="ct">
+        <img src="./icon/0411.jpg" alt=""><img src="./icon/0412.jpg" alt="">
+    </div>
